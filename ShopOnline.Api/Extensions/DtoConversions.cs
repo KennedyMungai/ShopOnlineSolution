dbd30d4db875a11ce83659b19f508139938ade5b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-09 14:54:03 +0300

    Added the missing fields

--- ShopOnline.Api/Extensions/DtoConversions.cs
+++ ShopOnline.Api/Extensions/DtoConversions.cs
@@ -16,9 +16,13 @@ namespace ShopOnline.Api.Extensions
                         Id = product.Id,
                         Name = product.Name,
                         Description = product.Description,
-                        ImageURL = product.ImageURL
+                        ImageURL = product.ImageURL,
+                        Price = product.Price,
+                        Qty = product.Qty,
+                        CategoryId = product.CategoryId,
+                        CategoryName = productCategory.Name
                     }
-                );
+                ).ToList();
         }
     }
 }
