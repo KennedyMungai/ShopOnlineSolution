a118b8011902f1e2ec4cf54fd5e3d1aee1d00b2e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-09 13:38:44 +0300

    Added sbSet for the users entity

--- ShopOnline.Api/Data/ShopOnlineDbContext.cs
+++ ShopOnline.Api/Data/ShopOnlineDbContext.cs
@@ -6,14 +6,10 @@ namespace ShopOnline.Api.Data
     public class ShopOnlineDbContext : DbContext
     {
         public DbSet<Cart>? Carts { get; set; }
-        
         public DbSet<CartItem>? CartItems { get; set; }
-        
         public DbSet<Product>? Products { get; set; }
-        
         public DbSet<ProductCategory>? ProductCategories { get; set; }
-        
-        
+        public DbSet<User>? Users { get; set; }
 
         public ShopOnlineDbContext(DbContextOptions<ShopOnlineDbContext> options) : base(options)
         {
