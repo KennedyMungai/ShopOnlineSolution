bad31a2203ed444aa93c0711b6b301f9fc6b452f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-10 11:31:14 +0300

    Added a function for getting a single item

--- ShopOnline.Api/Controllers/ProductController.cs
+++ ShopOnline.Api/Controllers/ProductController.cs
@@ -40,5 +40,28 @@ namespace ShopOnline.Api.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from the database");
             }
         }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<ProductDto>> GetItem(int id)
+        {
+            try
+            {
+                var product = await this.productRepository.GetItem(id);
+
+                if (product == null)
+                {
+                    return BadRequest();
+                }
+                else
+                {
+                    
+                    return Ok(productDtos);
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from the database");
+            }
+        }
     }
 }
