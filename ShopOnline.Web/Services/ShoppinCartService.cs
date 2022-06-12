280cd2d4e45a7f39fafae93f411d8757710b21f5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-12 15:15:52 +0300

    Added UpdateItem method

--- ShopOnline.Web/Services/ShoppinCartService.cs
+++ ShopOnline.Web/Services/ShoppinCartService.cs
@@ -1,7 +1,9 @@
-﻿using ShopOnline.Models.Dtos;
+﻿using Newtonsoft.Json;
+using ShopOnline.Models.Dtos;
 using ShopOnline.Web.Services.Contracts;
 using System.Collections.Generic;
 using System.Net.Http.Json;
+using System.Text;
 
 namespace ShopOnline.Web.Services
 {
@@ -82,6 +84,29 @@ namespace ShopOnline.Web.Services
                 }
 
                 return default(CartItemDto);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<CartItemDto> UpdateItem(CartItemQtyUpdateDto cartItemQtyUpdateDto)
+        {
+            try
+            {
+                var jsonRequest = JsonConvert.SerializeObject(cartItemQtyUpdateDto);
+                var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json-patch+json");
+
+                var response = await httpClient.PatchAsync($"api/ShoppingCart/{cartItemQtyUpdateDto.CartItemId}", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadFromJsonAsync<CartItemDto>();
+                }
+
+                return null;
             }
             catch (Exception)
             {
