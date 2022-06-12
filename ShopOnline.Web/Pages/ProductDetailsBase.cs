1943789c6f1145a36c91a7689face9afb6de7547
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-12 11:49:25 +0300

    Added the AddToCart_Click method

--- ShopOnline.Web/Pages/ProductDetailsBase.cs
+++ ShopOnline.Web/Pages/ProductDetailsBase.cs
@@ -9,7 +9,8 @@ namespace ShopOnline.Web.Pages
         [Parameter]
         public int Id { get; set; }
         [Inject]
-        public IProductService? ProductService { get; set; }
+        public IProductService? ProductService { get; set; } [Inject]
+        public IShoppingCartService? ShoppingCartService { get; set; }
         public ProductDto? Product { get; set; }
         public string? ErrorMessage { get; set; }
 
@@ -22,6 +23,19 @@ namespace ShopOnline.Web.Pages
             catch (Exception ex)
             {
                 ErrorMessage = ex.Message;
+            }
+        }
+
+        protected async Task AddToCart_Click(CartItemToAddDto cartItemToAddDto)
+        {
+            try
+            {
+                var cartItemDto = await ShoppingCartService.AddItem(cartItemToAddDto);
+            }
+            catch (Exception)
+            {
+
+                //Log Exception
             }
         }
     }
