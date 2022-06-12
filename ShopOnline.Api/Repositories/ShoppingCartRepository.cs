427eb63b8421fe6b8798a023b6a1f68a812bb299
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-12 13:35:24 +0300

    Implemented the DeleteItem code

--- ShopOnline.Api/Repositories/ShoppingCartRepository.cs
+++ ShopOnline.Api/Repositories/ShoppingCartRepository.cs
@@ -25,13 +25,13 @@ namespace ShopOnline.Api.Repositories
             if (await CartItemExists(cartItemToAddDto.CartId, cartItemToAddDto.ProductId) == false)
             {
                 var item = await (from product in this.shopOnlineDbContext.Products
-                                    where product.Id == cartItemToAddDto.ProductId
-                                    select new CartItem
-                                    {
-                                        CartId = cartItemToAddDto.CartId,
-                                        ProductId = product.Id,
-                                        Qty = cartItemToAddDto.Qty
-                                    }).SingleOrDefaultAsync();
+                                  where product.Id == cartItemToAddDto.ProductId
+                                  select new CartItem
+                                  {
+                                      CartId = cartItemToAddDto.CartId,
+                                      ProductId = product.Id,
+                                      Qty = cartItemToAddDto.Qty
+                                  }).SingleOrDefaultAsync();
 
                 if (item != null)
                 {
@@ -44,39 +44,47 @@ namespace ShopOnline.Api.Repositories
             return null;
         }
 
-        public Task<CartItem> DeleteItem(int id)
+        public async Task<CartItem> DeleteItem(int id)
         {
-            throw new NotImplementedException();
+            var item = await this.shopOnlineDbContext.CartItems.FindAsync(id);
+
+            if (item != null)
+            {
+                this.shopOnlineDbContext.CartItems.Remove(item);
+                await this.shopOnlineDbContext.SaveChangesAsync();
+            }
+
+            return item;
         }
 
         public async Task<CartItem> GetItem(int id)
         {
             return await (from cart in this.shopOnlineDbContext.Carts
-                            join cartItem in this.shopOnlineDbContext.CartItems
-                            on cart.Id equals cartItem.CartId
-                            where cartItem.Id == id
-                            select new CartItem
-                            {
-                                Id = cartItem.Id,
-                                ProductId = cartItem.ProductId,
-                                Qty = cartItem.Qty,
-                                CartId = cartItem.CartId
-                            }).SingleOrDefaultAsync();
+                          join cartItem in this.shopOnlineDbContext.CartItems
+                          on cart.Id equals cartItem.CartId
+                          where cartItem.Id == id
+                          select new CartItem
+                          {
+                              Id = cartItem.Id,
+                              ProductId = cartItem.ProductId,
+                              Qty = cartItem.Qty,
+                              CartId = cartItem.CartId
+                          }).SingleOrDefaultAsync();
         }
 
-        public async Task<IEnumerable<CartItem>> GetItems(int userId)
+        public async Task<List<CartItem>> GetItems(int userId)
         {
-            return await(from cart in this.shopOnlineDbContext.Carts
-                            join cartItem in this.shopOnlineDbContext.CartItems
-                            on cart.Id equals cartItem.CartId
-                            where cart.UserId == userId
-                            select new CartItem
-                            {
-                                Id = cartItem.Id,
-                                ProductId = cartItem.ProductId,
-                                Qty = cartItem.Qty,
-                                CartId = cartItem.CartId
-                            }).ToListAsync();
+            return await (from cart in this.shopOnlineDbContext.Carts
+                          join cartItem in this.shopOnlineDbContext.CartItems
+                          on cart.Id equals cartItem.CartId
+                          where cart.UserId == userId
+                          select new CartItem
+                          {
+                              Id = cartItem.Id,
+                              ProductId = cartItem.ProductId,
+                              Qty = cartItem.Qty,
+                              CartId = cartItem.CartId
+                          }).ToListAsync();
         }
 
         public Task<CartItem> UpdateQty(int id, CartItemQtyUpdateDto cartItemUpdateDto)
