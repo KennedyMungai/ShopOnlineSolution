52424eeffa0ff95971e5bf0c075fbf03746bc842
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-14 12:38:16 +0300

    Added GetItemsByCategory method template

--- ShopOnline.Web/Services/Contracts/IProductService.cs
+++ ShopOnline.Web/Services/Contracts/IProductService.cs
@@ -7,4 +7,5 @@ public interface IProductService
     Task<IEnumerable<ProductDto>> GetItems();
     Task<ProductDto> GetItem(int id);
     Task<IEnumerable<ProductCategoryDto>> GetProductCategories();
+    Task<IEnumerable<ProductDto>> GetItemsByCategory(int categoryId);
 }
