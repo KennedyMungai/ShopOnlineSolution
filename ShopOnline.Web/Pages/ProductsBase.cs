da788d71a5dd8296f86ab528d95a62418ebaa4c1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-14 12:36:32 +0300

    Removed the nullable symbols and used  tab spaces

--- ShopOnline.Web/Pages/ProductsBase.cs
+++ ShopOnline.Web/Pages/ProductsBase.cs
@@ -7,9 +7,9 @@ namespace ShopOnline.Web.Pages
     public class ProductsBase : ComponentBase
     {
         [Inject]
-        public IProductService? ProductService { get; set; }[Inject]
-        public IShoppingCartService? ShoppingCartService { get; set; }
-        public IEnumerable<ProductDto>? Products { get; set; }
+        public IProductService ProductService { get; set; }[Inject]
+        public IShoppingCartService ShoppingCartService { get; set; }
+        public IEnumerable<ProductDto> Products { get; set; }
         public string? ErrorMessage { get; set; }
 
         protected override async Task OnInitializedAsync()
@@ -32,9 +32,9 @@ namespace ShopOnline.Web.Pages
         protected IOrderedEnumerable<IGrouping<int, ProductDto>> GetGroupedProductsByCategory()
         {
             return from product in Products
-                   group product by product.CategoryId into prodByCatGroup
-                   orderby prodByCatGroup.Key
-                   select prodByCatGroup;
+                    group product by product.CategoryId into prodByCatGroup
+                    orderby prodByCatGroup.Key
+                    select prodByCatGroup;
         }
 
         protected string GetCategoryName(IGrouping<int, ProductDto> groupedProductDtos)
